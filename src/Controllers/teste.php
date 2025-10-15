58b626aa764162f8d1cc701a2666bfbd0ee45d2a
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-10-15 14:05:17 -0300

    first commit

--- src/Controllers/teste.php
+++ src/Controllers/teste.php
@@ -4,4 +4,5 @@ require_once __DIR__ . "/../Services/rds_station/core/delete_lead.php";
 
 header('Content-Type: text/plain; charset=utf-8');
 
- rd_delete_contact_by_email("otaviodeveloper6v@gmail.com");
+ rd_delete_contact_by_email("cancelado@example.com");
+ rd_delete_contact_by_email("ana.teste@example.com");
