1b4fbe2f2d80d047af4c793625cd5dd628a830ed
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-10-15 00:33:02 -0300

    first commit

---
+++ rds_station_module.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once __DIR__ . '/hooks.php';
+
+function rds_station_module_config() { 
+    return array(
+        'name' => 'RDS Station Integration For WHCMS',
+        'description' => 'Módulo responsável pela integração do RDS Station com o WHMCS.',
+        'version' => '1.0',
+        'author' => 'Sourei',
+        'fields' => array()
+    );
+}
+
+function rds_station_module_activate() {
+    return array('status' => 'success', 'description' => 'Módulo ativado com sucesso!');
+}
+
+function rds_station_module_deactivate() {
+    return array('status' => 'success', 'description' => 'Módulo desativado com sucesso!');
+}
+
+function rds_station_module_output() {
+    echo config();
+}
+
+
+
+?>
