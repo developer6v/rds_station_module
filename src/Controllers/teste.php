1330894477f9d241c70f5b8e6eca70ee1fc1ab56
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-10-15 01:53:09 -0300

    first commit

--- src/Controllers/teste.php
+++ src/Controllers/teste.php
@@ -1,6 +1,6 @@
 <?php
 include_once('../../../../../init.php');
-require_once __DIR__ . "/../Services/authentication/refresh_token.php";
+require_once __DIR__ . "/../Services/rds_station/authentication/refresh_token.php";
 
 use WHMCS\Database\Capsule;
 
