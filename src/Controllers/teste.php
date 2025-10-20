55661d3f8027aa5da71a7ebe4ab91f04b914913d
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-10-20 08:41:34 -0300

    update

--- src/Controllers/teste.php
+++ src/Controllers/teste.php
@@ -4,4 +4,4 @@ require_once __DIR__ . "/../Services/rds_station/conversao/cancelado.php";
 
 header('Content-Type: text/plain; charset=utf-8');
 
-echo (rd_send_api_cliente_cancelado("cancelado@example.com"));
+echo (rd_send_api_cliente_cancelado("teste@example.com"));
