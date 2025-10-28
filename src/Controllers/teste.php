6640146705a50c28a7e7da3fcc51cba3ddac9221
Otávio de Oliveira <otaviodeveloper6v@gmail.com>
2025-10-28 02:37:43 -0300

    commit de mudanças

--- src/Controllers/teste.php
+++ src/Controllers/teste.php
@@ -1,7 +1,20 @@
 <?php
 
+// Garante que o WHMCS esteja inicializado
 $init = realpath(__DIR__ . '/../../../../../init.php');
+if ($init && file_exists($init)) {
+    require_once $init;
+} else {
+    die("init.php não encontrado.\n");
+}
 
+// Inclui sua função personalizada
 require_once __DIR__ . "/../Services/wprocketelementor/sendtoscript.php";
 
-send_to_script_wprocketelementor("te");
+// Teste: use um ID de cliente real (ex: 1)
+$testUserId = 1;
+
+$result = send_to_script_wprocketelementor($testUserId);
+
+echo "Resposta:\n";
+var_dump($result);
